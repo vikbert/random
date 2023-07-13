c5e728992ecb6aba09fcbb8cee052e8758afd844
vikbert <segentor@gmail.com>
2023-07-13 10:34:16 +0200

    updates

--- cmd/random/main.go
+++ cmd/random/main.go
@@ -5,13 +5,6 @@ import (
 	"github.com/vikbert/random/pkg/command"
 )
 
-var (
-	goVersion = "unknown"
-	version   = "master"
-	commit    = "?"
-	date      = ""
-)
-
 func main() {
 	err := command.ExecuteAll()
 	if err != nil && err.Error() != "" {
